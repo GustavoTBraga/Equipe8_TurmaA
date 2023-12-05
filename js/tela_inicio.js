5a321f134bc269547e53d31515043510958c9ca5
Fernando Lima <fer.cilento@gmail.com>
2023-12-05 17:15:56 -0300

    .

--- js/tela_inicio.js
+++ js/tela_inicio.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.documentElement.requestFullscreen();
-
     const btn_novo_cadastro = document.querySelector('.cadastro')
 
     function Click_cadastro(){
         console.log(localStorage)
         localStorage.clear()
         console.log(localStorage)
+        document.documentElement.requestFullscreen();
     }
     
     btn_novo_cadastro.addEventListener('click', Click_cadastro)
